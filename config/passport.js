f00b03f03ecd7963f230693953af6840b1b7681d
Mohamed Fawzy <mofawzy1996@gmail.com>
2019-06-11 01:03:59 +0200

    Add JWT Strategy

---
+++ config/passport.js
@@ -0,0 +1,13 @@
+const JWTStrategy = require("passport-jwt").Strategy;
+const ExtractJWT = require("passport-jwt").ExtractJwt;
+const mongoose = require("mongoose");
+const User = mongoose.model("users");
+const keys = require("../config/keys");
+
+const opts = {};
+opts.jwtFromRequest = ExtractJWT.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = keys.secretOrKey;
+
+module.exports = passport => {
+  passport.use(new JWTStrategy(opts, (jwt_payload, done) => {}));
+};
