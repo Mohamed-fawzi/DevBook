bce5cedb982551776f294d8cc90767f24551cd74
Mohamed Fawzy <mofawzy1996@gmail.com>
2019-06-11 00:31:48 +0200

    Add login functionality

--- routes/api/users.js
+++ routes/api/users.js
@@ -48,4 +48,29 @@ router.post("/register", (req, res) => {
   });
 });
 
+// @route   GET api/users/login
+// @desc    Login a user / Returning a JWT
+// @access  Public
+router.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // Find the user by email
+  User.findOne({ email }).then(user => {
+    // Check for the user
+    if (!user) {
+      return res.status(404).json({ email: "User not found" });
+    }
+
+    // Check the password
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        res.json({ msg: "Success!" });
+      } else {
+        return res.status(400).json({ password: "Password incorrect" });
+      }
+    });
+  });
+});
+
 module.exports = router;
