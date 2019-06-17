6b45032e0c0704ebfcf7c0a20f1a3e6a8db7465a
Mohamed Fawzy <mofawzy1996@gmail.com>
2019-06-17 20:08:17 +0200

    Add - Delete User Route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -284,4 +284,21 @@ router.delete(
   }
 );
 
+// @route   DELETE api/profile/
+// @desc    Delete User and Profile
+// @access  Private
+router.delete(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOneAndRemove({ user: req.user.id })
+      .then(() => {
+        User.findByIdAndRemove({ _id: req.user.id }).then(() => {
+          res.json({ success: true });
+        });
+      })
+      .catch(err => res.status(404).json(err));
+  }
+);
+
 module.exports = router;
