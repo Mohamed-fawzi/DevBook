90b8c20e2229120f0d5d450454bbb25b05dd2d70
Mohamed Fawzy <mofawzy1996@gmail.com>
2019-06-28 21:57:36 +0200

    Update - Add Education component

--- client/src/components/add-credentials/AddEducation.js
+++ client/src/components/add-credentials/AddEducation.js
@@ -16,7 +16,7 @@ class AddEducation extends Component {
       from: "",
       to: "",
       current: false,
-      desciption: "",
+      description: "",
       errors: {},
       disabled: ""
     };
@@ -41,7 +41,7 @@ class AddEducation extends Component {
       from: this.state.from,
       to: this.state.to,
       current: this.state.current,
-      desciption: this.state.desciption
+      description: this.state.description
     };
 
     this.props.addEducation(eduData, this.props.history);
