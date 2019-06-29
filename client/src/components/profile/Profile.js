7b9266f30f5cb9dd372ed0d252c878ec844137aa
Mohamed Fawzy <mofawzy1996@gmail.com>
2019-06-29 19:54:43 +0200

    Add Initial Profile component

---
+++ client/src/components/profile/Profile.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import Spinner from "../common/Spinner";
+import { getProfileByHandle } from "../../actions/profileActions";
+
+class Profile extends Component {
+  componentDidMount() {
+    // Get the handle from the URL
+    const handle = this.props.match.params.handle;
+    if (handle) {
+      this.props.getProfileByHandle(handle);
+    }
+  }
+
+  render() {
+    return <div />;
+  }
+}
+
+Profile.propTypes = {
+  profile: PropTypes.object.isRequired
+};
+
+const mapStateToProps = (state = {
+  profile: state.profile
+});
+
+export default connect(mapStateToProps)(Profile);
