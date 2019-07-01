b135ac64922b964700163f7e99f63a64008debe7
Mohamed Fawzy <mofawzy1996@gmail.com>
2019-07-01 15:45:46 +0200

    Update Post actions

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-import { ADD_POST, GET_ERRORS } from "./types";
+import { ADD_POST, GET_ERRORS, GET_POSTS, POST_LOADING } from "./types";
 
 // Add Post
 export const addPost = postData => dispatch => {
@@ -16,6 +16,32 @@ export const addPost = postData => dispatch => {
       dispatch({
         type: GET_ERRORS,
         payload: err.response.data
+      })
+    );
+};
+
+// Set Loading State
+export const setPostLoading = () => {
+  return {
+    type: POST_LOADING
+  };
+};
+
+// Get Posts
+export const getPosts = () => dispatch => {
+  dispatch(setPostLoading());
+  axios
+    .get("/api/posts")
+    .then(res =>
+      dispatch({
+        type: GET_POSTS,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_POSTS,
+        payload: null
       })
     );
 };
