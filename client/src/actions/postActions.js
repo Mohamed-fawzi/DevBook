0a059906cae148a965a29a5bfa4c3567af02c04c
Mohamed Fawzy <mofawzy1996@gmail.com>
2019-07-01 18:19:58 +0200

    Update Post Actions

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -5,7 +5,6 @@ import {
   GET_ERRORS,
   GET_POSTS,
   POST_LOADING,
-  UPDATE_POST,
   DELETE_POST
 } from "./types";
 
@@ -53,24 +52,23 @@ export const getPosts = () => dispatch => {
     );
 };
 
-// // Update Post
-// export const updatePost = (id, postData) => dispatch => {
-//   dispatch(setPostLoading());
-//   axios
-//     .put(`/api/posts/${id}`, postData)
-//     .then(res =>
-//       dispatch({
-//         type: UPDATE_POST,
-//         payload: res.data
-//       })
-//     )
-//     .catch(err =>
-//       dispatch({
-//         type: GET_ERRORS,
-//         payload: err.response.data
-//       })
-//     );
-// };
+// Delete Post
+export const deletePost = id => dispatch => {
+  axios
+    .delete(`/api/posts/${id}`)
+    .then(res =>
+      dispatch({
+        type: DELETE_POST,
+        payload: id
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
 
 // Add Like
 export const addLike = id => dispatch => {
@@ -85,30 +83,12 @@ export const addLike = id => dispatch => {
       })
     );
 };
+
 // Remove Like
 export const removeLike = id => dispatch => {
-  dispatch(setPostLoading());
   axios
     .post(`/api/posts/unlike/${id}`)
     .then(res => dispatch(getPosts()))
-    .catch(err =>
-      dispatch({
-        type: GET_ERRORS,
-        payload: err.response.data
-      })
-    );
-};
-
-// Delete Post
-export const deletePost = id => dispatch => {
-  axios
-    .delete(`/api/posts/${id}`)
-    .then(res =>
-      dispatch({
-        type: DELETE_POST,
-        payload: id
-      })
-    )
     .catch(err =>
       dispatch({
         type: GET_ERRORS,
